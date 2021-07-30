f497f6686514e7367be873f39553c5501d9f0f3b
John Eisenmann <esperantanaso@gmail.com>
2021-07-29 22:18:23 -0400

    Extract arg variable definitions and return types

--- src/functionDefinition.ts
+++ src/functionDefinition.ts
@@ -2,6 +2,8 @@
 import { Displayable } from "./interfaces.js";
 import { StatementBlock } from "./statementBlock.js";
 import { Identifier } from "./identifier.js";
+import { Expression } from "./expression.js";
+import { ArgVariableDefinition } from "./variableDefinition.js";
 
 export abstract class FunctionDefinition implements Displayable {
     block: StatementBlock;
@@ -22,14 +24,41 @@ export abstract class FunctionDefinition implements Displayable {
 
 export class IdentifierFunctionDefinition extends FunctionDefinition {
     identifier: Identifier;
+    argVariableDefinitions: ArgVariableDefinition[];
+    returnTypeExpression: Expression;
     
     constructor(identifier: Identifier, block: StatementBlock) {
         super(block);
         this.identifier = identifier;
+        this.argVariableDefinitions = [];
+        this.returnTypeExpression = null;
+        this.block.processStatements((statement) => {
+            const { directive } = statement.type;
+            if (directive === "ARG") {
+                const identifier = statement.args[0].evaluateToIdentifier();
+                const definition = new ArgVariableDefinition(identifier, statement.args[1]);
+                this.argVariableDefinitions.push(definition);
+                return [];
+            } else if (directive === "RET_TYPE") {
+                if (this.returnTypeExpression !== null) {
+                    throw statement.createError("Extra RET_TYPE statement.");
+                }
+                this.returnTypeExpression = statement.args[0];
+                return [];
+            }
+            return null;
+        });
     }
     
     getDisplayStringHelper(): string {
-        return `Function ${this.identifier.getDisplayString()}`;
+        const output = [`Function ${this.identifier.getDisplayString()}`];
+        this.argVariableDefinitions.forEach((definition) => {
+            output.push(definition.getDisplayString());
+        });
+        if (this.returnTypeExpression !== null) {
+            output.push("Return type: " + this.returnTypeExpression.getDisplayString());
+        }
+        return output.join("\n");
     }
 }
 
---
+++ src/variableDefinition.ts
@@ -0,0 +1,29 @@
+
+import { Displayable } from "./interfaces.js";
+import { Identifier } from "./identifier.js";
+import { Expression } from "./expression.js";
+
+export abstract class VariableDefinition implements Displayable {
+    identifier: Identifier;
+    typeExpression: Expression;
+    
+    constructor(identifier: Identifier, typeExpression: Expression) {
+        this.identifier = identifier;
+        this.typeExpression = typeExpression;
+    }
+    
+    abstract getDisplayStringHelper(): string;
+    
+    getDisplayString(): string {
+        return `${this.getDisplayStringHelper()} ${this.identifier.getDisplayString()}, ${this.typeExpression.getDisplayString()}`;
+    }
+}
+
+export class ArgVariableDefinition extends VariableDefinition {
+    
+    getDisplayStringHelper(): string {
+        return "ARG";
+    }
+}
+
+
